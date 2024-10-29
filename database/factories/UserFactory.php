bcd2c9b9cd889c74f35670e412609aa3f7f6f050
sob1rdev <20sobirjon05@gmail.com>
2024-10-29 19:40:40 +0500

    chore: update UserFactory to support new user attributes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,9 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'phone' => '+998' .  fake()->numerify('(##)-###-##-##'),
+            'position' => fake()->jobTitle,
+            'gender' => fake()->randomElement(['male', 'female']),
         ];
     }
 
