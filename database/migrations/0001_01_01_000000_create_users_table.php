d2d1fc3ac60dd4f586fab42db0d127e84059ae1c
sob1rdev <20sobirjon05@gmail.com>
2024-10-29 19:40:36 +0500

    refactor: update User model and migration for additional fields

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->enum('gender', ['male', 'female'])->default('male')->after('email');
+            $table->string('position')->after('gender');
             $table->string('password');
+            $table->string('phone')->nullable()->unique()->after('password');
+            $table->timestamp('email_verified_at')->nullable()->after('role_id');
             $table->rememberToken();
             $table->timestamps();
         });
