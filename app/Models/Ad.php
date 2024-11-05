a9af28bc37b036db59433626bcee1777e80935c9
sob1rdev <20sobirjon05@gmail.com>
2024-11-05 12:11:19 +0500

    feat(Ad): create Ad model

---
+++ app/Models/Ad.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ad extends Model
+{
+    /** @use HasFactory<\Database\Factories\AdFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'user_id',
+        'brand_id',
+        'status_id',
+        'address',
+        'gender',
+        'price',
+        'rooms'
+    ];
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function branch(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Branch::class);
+    }
+
+    public function status(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Status::class);
+    }
+}
