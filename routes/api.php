eca01aa6f4695392df27a01a96ac507e51f9bf00
sob1rdev <20sobirjon05@gmail.com>
2024-11-05 19:00:45 +0500

    chore(routes): update api.php routes to support new resource controllers

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,27 @@
 <?php
 
-use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AdController;
+use App\Http\Controllers\Api\AdImageController;
+use App\Http\Controllers\Api\BranchController;
+use App\Http\Controllers\Api\RoleController;
+use App\Http\Controllers\Api\StatusController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('users', UserController::class)
+    ->middleware('auth:sanctum');
+
+Route::resource('ads', AdController::class)
+    ->middleware('auth:sanctum');
+
+Route::resource('ads_images', AdImageController::class)
+    ->middleware('auth:sanctum');
+
+Route::resource('branches', BranchController::class)
+    ->middleware('auth:sanctum');
+
+Route::resource('statuses', StatusController::class)
+    ->middleware('auth:sanctum');
+
+Route::resource('roles', RoleController::class)
     ->middleware('auth:sanctum');
