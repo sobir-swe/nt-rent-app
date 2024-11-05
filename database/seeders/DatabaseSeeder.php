ecf848ddbcd81db69be1aa8b34116c6a6b531a76
sob1rdev <20sobirjon05@gmail.com>
2024-11-05 15:34:48 +0500

    fix(DatabaseSeeder): update seeder to include new seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             BranchSeeder::class,
             RoleSeeder::class,
             AdSeeder::class,
+            AdImageSeeder::class,
         ]);
     }
 }
